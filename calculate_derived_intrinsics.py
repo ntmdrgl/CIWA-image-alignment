ad0a1f89f3ef5415cf5fcd4de77fa709d0e697f3
Nathaniel Madrigal <nthnlmdrgl@gmail.com>
2026-09-11 01:39:26 -0700

    tested flir extractor

--- calculate_derived_intrinsics.py
+++ calculate_derived_intrinsics.py
@@ -2,8 +2,8 @@ import json
 import numpy as np
 
 def calculate_derived_intrinsics(H, W, HFOV, VFOV):
-    fx = (W / 2) / np.tan(HFOV / 2)
-    fy = (H / 2) / np.tan(VFOV / 2)
+    fx = (W / 2) / np.tan(HFOV / 2).item()
+    fy = (H / 2) / np.tan(VFOV / 2).item()
     cx = W / 2
     cy = H / 2
     return fx, fy, cx, cy
@@ -19,6 +19,14 @@ def main():
             intrinsics[camera]['HFOV'], 
             intrinsics[camera]['VFOV']
         )
+        print(f"Saved derived intrinsics for {camera} to camera_intrinsics.json:")
+        print(f"    fx: {intrinsics[camera]['derived_intrinsics'][0]}")
+        print(f"    fy: {intrinsics[camera]['derived_intrinsics'][1]}")
+        print(f"    cx: {intrinsics[camera]['derived_intrinsics'][2]}")
+        print(f"    cy: {intrinsics[camera]['derived_intrinsics'][3]}")
 
     with open('camera_intrinsics.json', 'w') as f:
         json.dump(intrinsics, f, indent=4)
+
+if __name__ == "__main__":
+    main()
