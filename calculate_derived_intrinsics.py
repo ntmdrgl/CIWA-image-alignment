e9b00cee3a31f6b767bf41ddb1cfac72b5dad7f7
ntmdrgl <nthnlmdrgl@gmail.com>
2026-09-18 13:38:52 -0700

    added affine transformation

--- calculate_derived_intrinsics.py
+++ calculate_derived_intrinsics.py
@@ -2,8 +2,8 @@ import json
 import numpy as np
 
 def calculate_derived_intrinsics(H, W, HFOV, VFOV):
-    fx = (W / 2) / np.tan(HFOV / 2).item()
-    fy = (H / 2) / np.tan(VFOV / 2).item()
+    fx = (W / 2) / np.tan(np.deg2rad(HFOV / 2)).item()
+    fy = (H / 2) / np.tan(np.deg2rad(VFOV / 2)).item()
     cx = W / 2
     cy = H / 2
     return fx, fy, cx, cy
@@ -13,17 +13,23 @@ def main():
         intrinsics = json.load(f)
 
     for camera in intrinsics:
-        intrinsics[camera]['derived_intrinsics'] = calculate_derived_intrinsics(
+        fx, fy, cx, cy = calculate_derived_intrinsics(
             intrinsics[camera]['H'], 
             intrinsics[camera]['W'], 
             intrinsics[camera]['HFOV'], 
             intrinsics[camera]['VFOV']
         )
+
+        intrinsics[camera]['fx'] = fx
+        intrinsics[camera]['fy'] = fy
+        intrinsics[camera]['cx'] = cx
+        intrinsics[camera]['cy'] = cy
+
         print(f"Saved derived intrinsics for {camera} to camera_intrinsics.json:")
-        print(f"    fx: {intrinsics[camera]['derived_intrinsics'][0]}")
-        print(f"    fy: {intrinsics[camera]['derived_intrinsics'][1]}")
-        print(f"    cx: {intrinsics[camera]['derived_intrinsics'][2]}")
-        print(f"    cy: {intrinsics[camera]['derived_intrinsics'][3]}")
+        print(f"    fx: {intrinsics[camera]['fx']}")
+        print(f"    fy: {intrinsics[camera]['fy']}")
+        print(f"    cx: {intrinsics[camera]['cx']}")
+        print(f"    cy: {intrinsics[camera]['cy']}")
 
     with open('camera_intrinsics.json', 'w') as f:
         json.dump(intrinsics, f, indent=4)
--- test_flir_extractor.py
+++ test_flir_extractor.py
@@ -7,35 +7,29 @@ import argparse
 import pathlib
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--plant", type=str, required=True, choices=["citrus"], help="Name of the plant dataset in data directory")
+parser.add_argument("--plant", type=str, required=True, help="Name of the plant dataset in data directory")
 parser.add_argument("--num_images", type=int, required=False, help="The number of images", default=1)
 parser.add_argument("--start_img", type=int, required=False, help="The starting image index", default=0)
 args = parser.parse_args()
 
-def main():
-    thermal_dir = pathlib.Path("data\\" + args.plant + "\\train_thermal")
+thermal_dir = pathlib.Path("data\\" + args.plant + "\\train_thermal")
 
-    flir = flir_image_extractor.FlirImageExtractor()
+flir = flir_image_extractor.FlirImageExtractor()
 
+for index, img in enumerate(thermal_dir.iterdir()):
+    if not img.is_file() or img.suffix != ".jpg":
+        continue
 
-    for index, img in enumerate(thermal_dir.iterdir()):
-        if not img.is_file() or img.suffix != ".jpg":
-            continue
+    if index >= args.num_images:
+        print("Finished extracting images")
+        break
 
-        if index >= args.num_images:
-            print("Finished extracting images")
-            break
+    flir.process_image(img)
+    
+    thermal_img = flir.get_thermal_np()
+    print(f"thermal_img: shape: {thermal_img.shape}, dtype: {thermal_img.dtype}")
+    
+    embedded_img = flir.get_rgb_np()
+    print(f"embedded_img: shape: {embedded_img.shape}, dtype: {embedded_img.dtype}")
 
-        flir.process_image(img)
-        
-        thermal_img = flir.get_thermal_np()
-        print(f"thermal_img: shape: {thermal_img.shape}, dtype: {thermal_img.dtype}")
-        
-        embedded_img = flir.get_rgb_np()
-        print(f"embedded_img: shape: {embedded_img.shape}, dtype: {embedded_img.dtype}")
-
-        flir.plot()
-        
-
-if __name__ == "__main__":
-    main()
+    flir.plot()
